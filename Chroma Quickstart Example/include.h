3899f452742f978f3f50f31ccc580839b8c8927d
Vaypron <t.n.gebauer@googlemail.com>
2017-04-22 11:49:27 +0200

    Edited include.h

--- Chroma Quickstart Example/include.h
+++ Chroma Quickstart Example/include.h
@@ -1,8 +1,3 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #include <iostream>
@@ -14,4 +9,4 @@
 #include "RzChromaSDKTypes.h"
 #include "RzErrors.h"
 
-// TODO: reference additional headers your program requires here
+
